5a98fc6278143bdf6f9247fa155691a1804a9909
Zroeth <gabriel.molina38@yahoo.com>
2020-10-06 15:34:40 -0600

    Clases

---
+++ Lab3/Huffman/Operacioens.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+
+namespace Huffman
+{
+    public class Operacioens
+    {
+        
+        public string path="";
+        public string root = "";
+      
+        public Operacioens(string path, string root)
+        {
+            this.path = path;
+            this.root = root;
+
+        }
+
+        public void Comprimir()
+        {
+            string root2 = root;
+            root = root + @"\\Escritoriovpn.hdef";
+            HuffmanEncodeFile hef = new HuffmanEncodeFile(@path, @root);
+            byte[] b = hef.Encode();
+            root = root2 + @"\\Upload\\Comprimido.huff";
+            File.WriteAllBytes(@root, b);
+        }
+
+        public void Descomporimir()
+        {
+            string root2 = root;
+            string root3 = root;
+            root = root + @"\\Upload\\Comprimido.huff";
+            byte[] fileBytes = FileHelper.GetArchivoBytes(@root);
+            root3 = root3 + @"\\Escritoriovpn.hdef";
+            string bb = HuffmanEncodeFile.Decode(fileBytes, @root3);
+            root = root2 + @"\\Upload\\DescomprimidoHuff.txt";
+            File.WriteAllText(@root, bb);
+        }
+    }
+}
